264d506bca7eb890cee7a2211540e0fc5d44ca7d
mengxichen <mengxi.chen.2006@gmail.com>
2016-03-03 20:02:21 -0800

    Assignment2

--- public/assignment/views/services/forms.service.client.js
+++ public/assignment/views/services/forms.service.client.js
@@ -10,7 +10,7 @@
         var forms =[
             {"_id": "000", "title": "Contacts", "userId": 123},
             {"_id": "010", "title": "ToDo",     "userId": 123},
-            {"_id": "020", "title": "CDs",      "userId": 234},
+            {"_id": "020", "title": "CDs",      "userId": 234}
         ];
 
 
